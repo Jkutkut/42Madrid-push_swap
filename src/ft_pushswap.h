8dca3b140a7735889cd02c28427e350e9a7ed1d8
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-19 13:17:07 +0100

    .h file created

---
+++ src/ft_pushswap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pushswap.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkutkut <jre-gonz@student.42madrid.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 12:58:56 by jkutkut           #+#    #+#             */
+/*   Updated: 2022/02/19 13:16:56 by jkutkut          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PUSHSWAP
+# define FT_PUSHSWAP
+
+#include "libft/libft.h"
+
+#ifndef DEBUG
+# define GRAPHIC 0
+#else
+# define GRAPHIC 1
+#endif
+
+
+
+#endif
