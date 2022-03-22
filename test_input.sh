e01b8f14a45d05e187513abb4af67902ff7d1197
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-22 11:49:31 +0100

    test input in progress

--- test_input.sh
+++ test_input.sh
@@ -1,13 +1,32 @@
-#!/bin/bash
+#!/bin/sh
+
+# Colors:
+NC='\033[0m' # No Color
+RED='\033[1;31m';
+YELLOW='\033[1;33m';
+GREEN='\033[1;32m';
+BLUE='\033[1;34m';
 
 executable=./ft_pushswap
 
 run_test() {
-	./ft_pushswap $@
+	echo "./ft_pushswap $@ 2> output.tmp"
+	./ft_pushswap $@ 2> output.tmp
+	cat output.tmp
+	if [ ! "$(cat output.tmp | grep 'Error')" = "" ]; then
+		echo "Error"
+	fi
+	rm output.tmp
 }
 
-input="test/invalid_input"
+input_file="test/invalid_input"
 while IFS= read -r line; do
-	run_test $line
-done < "$input"
+	input=$(echo $line | cut -d: -f2)
+	run_test $input
+	# if [ "$(run_test $input)" = "Error" ]; then
+	# 	echo "${GREEN}[OK]${NC}"
+	# else
+	# 	echo "${RED}[FAIL]${NC}"
+	# fi
+done < "$input_file"
 
