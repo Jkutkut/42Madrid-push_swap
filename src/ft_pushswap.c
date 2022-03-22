2a30fd9db7c61f912c68620813c5b5ca3896a07f
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-22 09:28:57 +0100

    basic parse implemented

--- src/ft_pushswap.c
+++ src/ft_pushswap.c
@@ -6,51 +6,70 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 17:31:20 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/21 22:48:01 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/22 09:28:20 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_pushswap.h"
 
-t_dstack	*init_pushswap(char *arg)
-{
-	t_dstack	*pswap;
-	int			i;
-	int		start;
-	int		len;
 
-	pswap = malloc(sizeof(t_dstack));
-	if (pswap == NULL)
-		return (NULL);
+void	parse_input(char *arg, t_dstack *pswap)
+{
+	int	i;
+	int	len;
+	int	start;
+
 	i = 0;
-	pswap->b = NULL;
 	len = ft_strlen(arg);
 	while (i < len)
 	{
-		while (ft_hasany(" ", arg[i]))
+		while (ft_hasany(" \t", arg[i]))
 			i++;
 		start = i;
 		printf("Start '%s'\n", arg + start);
 		while (ft_isdigit(arg[i]))
 			i++;
 		printf("end '%s' -> %d\n", arg + i, arg[i] - '\0');
-		if (!ft_hasany(" \0", arg[i]))
+		if (arg[i] != ' ' && arg[i] != '\0')
 			free_end(pswap, 1, "Error: invalid argument\n");
 		arg[i] = '\0';
+		printf("Atoi: '%s' -> %d\n", arg + start, ft_atoi(arg + start));
 		ft_lstadd_back(&pswap->a, ft_lstnew(ft_atoi(arg + start)));
 		if (i < len)
 			arg[i] = ' ';
 	}
+}
+
+t_dstack	*init_pushswap(int argv, char **arg)
+{
+	t_dstack	*pswap;
+	int			i;
+
+	pswap = malloc(sizeof(t_dstack));
+	if (pswap == NULL)
+		return (NULL);
+	pswap->b = NULL;
+	pswap->a = NULL;
+	i = 1;
+	while (i < argv)
+		parse_input(arg[i++], pswap);
 	return (pswap);
+}
+
+
+void	check_leaks(void)
+{
+	system("leaks -q ft_pushswap");
 }
 
 int	main(int argc, char **argv)
 {
 	t_dstack	*pswap;
-	
-	if (argc != 2)
+
+	atexit(check_leaks);
+	if (argc == 1)
 		return (1);
-	pswap = init_pushswap(argv[1]);
+	pswap = init_pushswap(argc, argv);
 	if (pswap == NULL)
 	{
 		printf("Error\n");
