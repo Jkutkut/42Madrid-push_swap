97ebd6e0b340d6d0cd3fa58fccee05b48d3e01a0
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-23 08:55:58 +0100

    fixed

--- src/tools/binary_search.c
+++ src/tools/binary_search.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 08:41:36 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/23 08:42:09 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/23 08:55:25 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	binary_search(int arr[], int l, int r, int value)
 		if (arr[m] == value)
 			return m;
 		if (arr[m] > value)
-			return binarySearch(arr, l, m - 1, value);
-		return binarySearch(arr, m + 1, r, value);
+			return binary_search(arr, l, m - 1, value);
+		return binary_search(arr, m + 1, r, value);
 	}
 	return -1;
 }
