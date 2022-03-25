8e063a28c7d1ee7a3a60243ec2caf41ab81bc489
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-26 00:55:10 +0100

    bug on rr preventing rrX to be executed fixed

--- src/bonus/checker.c
+++ src/bonus/checker.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 12:10:15 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/25 21:32:58 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/26 00:52:27 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	moves_remaining(char **s)
 {
 	*s = get_next_line(0);
+	printf("'%s'\n", *s);
 	if (*s == NULL)
 		return (0);
 	return (1);
@@ -24,38 +25,43 @@ int	main(int argc, char **argv)
 {
 	t_dstack	*pswap;
 	char		*line;
+	char		*result;
 
 	pswap = init_pushswap(argc, argv);
+	print(pswap);
 	while (moves_remaining(&line))
 	{
 		if (ft_strncmp(line, "sa", 2) == 0)
-			sa(pswap);
+			result = sa(pswap);
 		else if (ft_strncmp(line, "sb", 2) == 0)
-			sb(pswap);
+			result = sb(pswap);
 		else if (ft_strncmp(line, "ss", 2) == 0)
-			ss(pswap);
+			result = ss(pswap);
 		else if (ft_strncmp(line, "pa", 2) == 0)
-			pa(pswap);
+			result = pa(pswap);
 		else if (ft_strncmp(line, "pb", 2) == 0)
-			pb(pswap);
+			result = pb(pswap);
 		else if (ft_strncmp(line, "ra", 2) == 0)
-			ra(pswap);
+			result = ra(pswap);
 		else if (ft_strncmp(line, "rb", 2) == 0)
-			rb(pswap);
-		else if (ft_strncmp(line, "rr", 2) == 0)
-			rr(pswap);
+			result = rb(pswap);
+		else if (ft_strncmp(line, "rr\n", 3) == 0)
+			result = rr(pswap);
 		else if (ft_strncmp(line, "rra", 3) == 0)
-			rra(pswap);
+			result = rra(pswap);
 		else if (ft_strncmp(line, "rrb", 3) == 0)
-			rrb(pswap);
+			result = rrb(pswap);
 		else if (ft_strncmp(line, "rrr", 3) == 0)
-			rrr(pswap);
+			result = rrr(pswap);
 		else
 		{
 			free(line);
 			free_end(pswap, 1, "Command not found\n");
 		}
 		free(line);
+		if (!result)
+			free_end(pswap, 1, "Not able to execute the asked steps.\n");
+		print(pswap);
 	}
 	if (!is_sorted(pswap))
 		free_end(pswap, 1, "Stack still not sorted :S\n");
