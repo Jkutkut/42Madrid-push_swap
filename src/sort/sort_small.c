840afd064ad19ff641eaace1c4fa34b60f800b22
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-02 22:28:16 +0200

    5 logic refactored. Now it should cover more cases

--- src/sort/sort_small.c
+++ src/sort/sort_small.c
@@ -6,28 +6,59 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 17:08:51 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/02 20:51:49 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/02 22:27:52 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sort.h"
 
-static int	dist_to(int v, t_list *lst)
+static int	dist_to_sandwich(int v, t_list *lst)
 {
-	int		dist;
+	int 	dist;
 	t_list	*tmp;
+	t_list	*prev;
 
+	if (lst->content > v && ft_lstlast(lst)->content < v)
+		return (0);
 	dist = 0;
 	tmp = lst;
-	while (tmp && tmp->content < v)
+	while (tmp)
 	{
-		dist++;
+		prev = tmp;
 		tmp = tmp->next;
+		dist++;
+		if (tmp && tmp->content >= v && prev->content < v)
+			return (dist);
 	}
-	if (lst->content /**/&& tmp && tmp->content == v)
-		return (dist);
-	
-	return (dist);
+	printf("Error: dist_to_sandwich\n");
+	return (0);
+}
+
+static int	index_lst(int v, t_list *lst)
+{
+	int		i;
+	t_list	*tmp;
+
+	i = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		if (tmp->content == v)
+			return (i);
+		tmp = tmp->next;
+		i++;
+	}
+	printf("Error: index_lst\n");
+	return (0);
+}
+
+static int	dist_to(int v, t_list *lst)
+{
+	if (v <= get_from_lst(ft_min, lst))
+		return (index_lst(get_from_lst(ft_min, lst), lst));
+	if (v > get_from_lst(ft_max, lst))
+		return (ft_lstsize(lst)); // TODO Not tested
+	return (dist_to_sandwich(v, lst));
 }
 
 void	sort_5(t_dstack *pswap)
@@ -55,16 +86,16 @@ void	sort_5(t_dstack *pswap)
 			apply(pswap, PA);
 		}
 	}
-	// i = dist_to(0, pswap->a, 1);
-	// if (i < pswap->size / 2)
-	// 	while (i--)
-	// 		apply(pswap, RA);
-	// else
-	// {
-	// 	i = pswap->size - i;
-	// 	while (i--)
-	// 		apply(pswap, RRA);
-	// }
+	i = dist_to(0, pswap->a);
+	if (i < pswap->size / 2)
+		while (i--)
+			apply(pswap, RA);
+	else
+	{
+		i = pswap->size - i;
+		while (i--)
+			apply(pswap, RRA);
+	}
 }
 
 
