6eb3c811bf896e4d3514369e779b4d863ff60bdc
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-02 17:15:02 +0200

    sort from 0-3 fully handled

--- src/sort/sort_small.c
+++ src/sort/sort_small.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 17:08:51 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/27 18:59:04 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/02 17:09:15 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@ void	sort_3(t_dstack *pswap, int l)
 	lst = pswap->a;
 	if (l == 1)
 		lst = pswap->b;
+	if (ft_lstsize(lst) < 3)
+	{
+		if (ft_lstsize(lst) == 2 && lst->content > lst->next->content)
+			apply(pswap, SA + l);
+		return ;
+	}
 	if (lst->content == 0 && lst->next->content == 2)
 	{
 		apply(pswap, RRA + l);
@@ -41,5 +47,6 @@ void	sort_small(t_dstack *pswap)
 {
 	if (pswap->size > 5)
 		free_end(pswap, 0, "Not enough elements to sort.\n");
-	sort_3(pswap, 0);
+	if (pswap->size <= 3)
+		sort_3(pswap, 0);
 }
