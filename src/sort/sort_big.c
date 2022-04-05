ab249c33d9efe78c956691b4d31aab5b8b61176a
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-05 13:52:43 +0200

    0..40

--- src/sort/sort_big.c
+++ src/sort/sort_big.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 13:35:15 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/05 13:09:13 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/05 13:35:28 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,28 +42,30 @@ static void	move_subgroup(t_dstack *pswap, int group)
 	int max;
 	int dir;
 
-	max = (group + 1) * (pswap->size / GROUPS) - 1;
+	// max = (group + 1) * (pswap->size / GROUPS) - 1;
 	min = group * (pswap->size / GROUPS);
-	max = ft_min(get_from_lst(ft_max, pswap->b), max);
-	dir = 0;
-	while (min < max)
-	{
-		if (pswap->b->content == max)
-		{
-			apply(pswap, PA);
-			max--;
-		}
-		else
-			apply(pswap, RB + dir * 3);
-		if (pswap->b->content < min)
-		{
-			dir = (dir + 1) % 2;
-			if (pswap->b->next->content < min)
-				dir = 1;
-			apply(pswap, RB + dir * 3);
-		}
-	}
-	apply(pswap, PA);
+	// max = ft_max(get_from_lst(ft_max, pswap->b), max);
+	max = get_from_lst(ft_max, pswap->b);
+	printf("min: %d, max: %d\n  (should be %d)\n", min, max, (group + 1) * (pswap->size / GROUPS) - 1);
+	// dir = 0;
+	// while (min < max)
+	// {
+	// 	if (pswap->b->content == max)
+	// 	{
+	// 		apply(pswap, PA);
+	// 		max--;
+	// 	}
+	// 	else
+	// 		apply(pswap, RB + dir * 3);
+	// 	if (pswap->b->content < min)
+	// 	{
+	// 		dir = (dir + 1) % 2;
+	// 		if (pswap->b->next->content < min)
+	// 			dir = 1;
+	// 		apply(pswap, RB + dir * 3);
+	// 	}
+	// }
+	// apply(pswap, PA);
 }
 
 static void	back_to_a(t_dstack *pswap)
