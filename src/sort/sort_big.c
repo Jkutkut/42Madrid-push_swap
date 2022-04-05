4901c1516cbb4c61a492d62b98a4471e81876aef
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-05 13:10:30 +0200

    minor refactor and optimization of big_sort

--- src/sort/sort_big.c
+++ src/sort/sort_big.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 13:35:15 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/05 13:06:33 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/05 13:09:13 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,41 +39,28 @@ static void	to_b(t_dstack *pswap)
 static void	move_subgroup(t_dstack *pswap, int group)
 {
 	int min;
-	int mid;
 	int max;
 	int dir;
 
 	max = (group + 1) * (pswap->size / GROUPS) - 1;
 	min = group * (pswap->size / GROUPS);
-	mid = (min + max) / 2;
-
 	max = ft_min(get_from_lst(ft_max, pswap->b), max);
-	printf("group %d\n  - min: %d\n  - mid: %d\n  - max: %d\n", group, min, mid, max);
-	printf("Current values: %d\n", pswap->b->content);
-	fflush(stdout);
-
 	dir = 0;
-	// while (min < get_from_lst(ft_max, pswap->b))
-	// while (min < max && pswap->b->content >= min && pswap->b->content <= max)
 	while (min < max)
 	{
 		if (pswap->b->content == max)
 		{
 			apply(pswap, PA);
 			max--;
-			printf("Now is %d\n", pswap->b->content);
-			fflush(stdout);
 		}
 		else
 			apply(pswap, RB + dir * 3);
 		if (pswap->b->content < min)
 		{
-			printf("  min detected\n");
 			dir = (dir + 1) % 2;
-			if (pswap->b->next->content < mid)
+			if (pswap->b->next->content < min)
 				dir = 1;
 			apply(pswap, RB + dir * 3);
-			printf("  dir %d -> rotate\n  v %d\n", dir, pswap->b->content);
 		}
 	}
 	apply(pswap, PA);
@@ -87,14 +74,11 @@ static void	back_to_a(t_dstack *pswap)
 	while (group-- > 0)
 	{
 		move_subgroup(pswap, group);
-		ft_putstr_fd("GROUP ENDED\n", 1);
-		// break;
 	}
 }
 
 void	sort_big(t_dstack *pswap)
 {
 	to_b(pswap);
-	printf("Size: %d\n", pswap->size);
 	back_to_a(pswap);
 }
