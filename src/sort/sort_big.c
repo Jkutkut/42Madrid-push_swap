77da7239d305128e5485c0b797c90f8c92ceeca2
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-05 13:07:27 +0200

    first long sort done!

--- src/sort/sort_big.c
+++ src/sort/sort_big.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 13:35:15 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/05 12:15:02 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/05 13:06:33 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,17 +41,42 @@ static void	move_subgroup(t_dstack *pswap, int group)
 	int min;
 	int mid;
 	int max;
+	int dir;
 
-	max = (group + 1) * (pswap->size / GROUPS);
-	min = max - (pswap->size / GROUPS);
-	mid = (min + max--) / 2;
+	max = (group + 1) * (pswap->size / GROUPS) - 1;
+	min = group * (pswap->size / GROUPS);
+	mid = (min + max) / 2;
+
+	max = ft_min(get_from_lst(ft_max, pswap->b), max);
 	printf("group %d\n  - min: %d\n  - mid: %d\n  - max: %d\n", group, min, mid, max);
+	printf("Current values: %d\n", pswap->b->content);
 	fflush(stdout);
-	while (pswap->b->content > mid)
-	{
-		apply(pswap, PA);
-	}
 
+	dir = 0;
+	// while (min < get_from_lst(ft_max, pswap->b))
+	// while (min < max && pswap->b->content >= min && pswap->b->content <= max)
+	while (min < max)
+	{
+		if (pswap->b->content == max)
+		{
+			apply(pswap, PA);
+			max--;
+			printf("Now is %d\n", pswap->b->content);
+			fflush(stdout);
+		}
+		else
+			apply(pswap, RB + dir * 3);
+		if (pswap->b->content < min)
+		{
+			printf("  min detected\n");
+			dir = (dir + 1) % 2;
+			if (pswap->b->next->content < mid)
+				dir = 1;
+			apply(pswap, RB + dir * 3);
+			printf("  dir %d -> rotate\n  v %d\n", dir, pswap->b->content);
+		}
+	}
+	apply(pswap, PA);
 }
 
 static void	back_to_a(t_dstack *pswap)
@@ -59,9 +84,9 @@ static void	back_to_a(t_dstack *pswap)
 	int	group;
 
 	group = GROUPS;
-	while (group >= 0)
+	while (group-- > 0)
 	{
-		move_subgroup(pswap, group--);
+		move_subgroup(pswap, group);
 		ft_putstr_fd("GROUP ENDED\n", 1);
 		// break;
 	}
