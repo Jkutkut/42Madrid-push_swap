b3584775694456fb0dc59a646d104edd7d4dbd52
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-14 21:27:55 +0200

    test_checker now can be used both form root directory and scripts directory

--- scripts/test_checker.sh
+++ scripts/test_checker.sh
@@ -7,12 +7,20 @@ YELLOW='\033[1;33m';
 GREEN='\033[1;32m';
 BLUE='\033[1;34m';
 
-test_location=".test/"
+repo_location=./
+if [ ! -f "$repo_location/Makefile" ]; then
+	repo_location=../
+fi
+
+test_location="${repo_location}.test/"
 test_input_prefix="${test_location}input_"
 test_output_prefix="${test_location}bonus/result_"
 test="basic 3elements"
 
-make bonus
+pushswap=${repo_location}pushswap
+checker=${repo_location}checker
+
+make -C ${repo_location} bonus
 
 for t in $(echo $test); do
 	i=1;
@@ -22,7 +30,7 @@ for t in $(echo $test); do
 		if [ $i -lt 10 ]; then
 			spectedResult=${test_output_prefix}${t}_0$i
 		fi
-		cat $spectedResult | ./checker $input 2> error.tmp > success.tmp
+		cat $spectedResult | $checker $input 2> error.tmp > success.tmp
 		if [ "$(cat success.tmp)" = "OK" ]; then
 			echo " ${GREEN}[OK]${NC}"
 		else
