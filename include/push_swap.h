18ca65ca1f790f5b61fcb72bb1200d45976eb3ae
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-16 08:03:21 +0200

    Ready to work on project

---
+++ include/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 12:58:56 by jkutkut           #+#    #+#             */
+/*   Updated: 2022/06/16 08:02:51 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PUSHSWAP
+# define FT_PUSHSWAP
+
+#include <unistd.h>
+#  include <stdio.h> // TODO REMOVE
+#include <stdlib.h>
+
+
+#endif
