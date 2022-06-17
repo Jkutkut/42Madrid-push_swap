d9f6a305c8a99909063f37ac5b4f5627ec5b347c
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-17 11:58:07 +0200

    Export

--- include/msg.h
+++ include/msg.h
@@ -1,32 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_pushswap.c                                    :+:      :+:    :+:   */
+/*   msg.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 09:33:20 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/17 11:16:04 by jre-gonz         ###   ########.fr       */
+/*   Created: 2022/06/17 11:15:12 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/06/17 11:16:07 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "input.h"
+#ifndef MSG_H
+#define MSG_H
 
-t_dstack	*init_pushswap(int argc, char **argv)
-{
-	t_dstack	*pswap;
-	int			i;
+#define ERROR_MALLOC "Malloc failed\n"
+#define  ""
+#define  ""
+#define  ""
+#define  ""
+#define  ""
 
-	pswap = malloc(sizeof(t_dstack));
-	if (pswap == NULL)
-		end(1, ERROR_MALLOC);
-	pswap->b = NULL;
-	pswap->a = NULL;
-	pswap->arg = NULL;
-	i = 1;
-	while (i < argc)
-		parse_input(argv[i++], pswap);
-	check_input(pswap);
-	normalize(pswap);
-	return (pswap);
-}
+#endif
