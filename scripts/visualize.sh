bf951989fc48524b9304cc7ba74418f6f430c01f
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-28 09:33:47 +0200

    Visualizer now on web and script updated

--- scripts/visualize.sh
+++ scripts/visualize.sh
@@ -1,67 +1,60 @@
 #!/bin/sh
 
-# repo_location=./
-# if [ ! -f "$repo_location/Makefile" ]; then
-# 	repo_location=../
-# fi
+repo_location=./
+if [ ! -f "$repo_location/Makefile" ]; then
+	repo_location=../
+fi
 
-# visualizer_dir=$repo_location.visualizer
+visualizer_dir=$repo_location.visualizer
 
-# check_dependencies() {
-# 	# Check python3 dependencies
-# 	if ! command -v python3 >/dev/null 2>&1; then
-# 		echo "Python3 is not installed. It can be installed with"
-# 		echo "sudo apt install python3"
-# 		return 1
-# 	fi
-# 	# python3 tkinter
-# 	if ! python3 -c "import tkinter" >/dev/null 2>&1; then
-# 		echo "Python3 tkinter is not installed. It can be installed with"
-# 		echo "sudo apt install python3-tk"
-# 		return 1
-# 	fi
-# 	return 0
-# }
+VISUALIZER() {
+	ARGS="$@";
+	open "https://GlaceCoding.github.io/tools/pushswap/visualizer/?size=15&speed=150&args=$ARGS&queue="$(./push_swap $ARGS | tr '\n' ',')
+}
 
-# small() {
-# 	python3 $visualizer_dir/pyviz.py 1 3 2
-# }
+visualizeRandom() {
+	min=$1
+	max=$2
+	VISUALIZER $(ruby -e "puts ($min..$max).to_a.shuffle.join(' ')")
+}
 
-# medium() {
-# 	python3 $visualizer_dir/pyviz.py 2 3 1 5 4
-# }
+small() {
+	visualizeRandom -1 1
+}
+
+medium() {
+	visualizeRandom -3 1
+}
 
 # big() {
-# 	input=$(ruby -e "puts (-19..20).to_a.shuffle.join(' ')")
-# 	python3 $visualizer_dir/pyviz.py $input
+# 	visualizeRandom -19 20
 # }
 
 # huge() {
-# 	input=$(ruby -e "puts (-190..200).to_a.shuffle.join(' ')")
-# 	python3 $visualizer_dir/pyviz.py $input
+# 	visualizeRandom -190 200
 # }
 
-# main() {
-# 	check_dependencies || return 1
-# 	# python3 $visualizer_dir/pyviz.py `ruby -e "puts (-200..200).to_a.shuffle.join(' ')"`
-# 	while [ ! "$1" = "" ]; do
-# 		if [ "$1" = "small" ]; then
-# 			small
-# 		elif [ "$1" = "medium" ]; then
-# 			medium
-# 		elif [ "$1" = "big" ]; then
-# 			big
-# 		elif [ "$1" = "huge" ]; then
-# 			huge
-# 		else
-# 			break
-# 		fi
-# 		shift
-# 	done
-# 	python3 $visualizer_dir/pyviz.py $@
-# }
+main() {
+	# check_dependencies || return 1
+	while [ ! "$1" = "" ]; do
+		if [ "$1" = "small" ]; then
+			small
+		elif [ "$1" = "medium" ]; then
+			medium
+		# max len of url is 2048 chars
+		# elif [ "$1" = "big" ]; then
+		# 	big
+		# elif [ "$1" = "huge" ]; then
+		# 	huge
+		else
+			VISUALIZER $@
+			break
+		fi
+		shift
+	done
+	
+}
 
-# make -C $repo_location
-# main $@
+make -C $repo_location
+main $@
 
-VISUALIZER() { ARGS="$@"; open "https://GlaceCoding.github.io/tools/pushswap/visualizer/?size=15&speed=150&args=$ARGS&queue="$(./push_swap $ARGS | tr '\n' ',') }
