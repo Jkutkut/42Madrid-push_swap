edb848f1d66b41687b040ec2008243a4d310af9c
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-29 10:47:53 +0200

    optimizing 5 Current: Fails: 0  KOs: 18  OKs: 55  Outstanding: 46

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 10:16:20 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/29 10:12:48 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/29 10:47:36 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 void	sort_5(t_dstack *pswap)
 {
 	int	s;
+	int	count;
 
 	s = pswap->size;
 	while (s-- > 3)
 		apply(pswap, PB);
 	sort_3(pswap, 0);
 
-	while (pswap->b)
+	int DEBUG = -1;
+	while (pswap->b && DEBUG)
 	{
+		DEBUG--;
 		int v = pswap->b->content;
-		if (v == ft_lstlast(pswap->a)->content + 1)
+		if (v > ft_lstlast(pswap->a)->content)
 		{
 			apply(pswap, PA);
 			apply(pswap, RA);
@@ -35,17 +38,22 @@ void	sort_5(t_dstack *pswap)
 		}
 		else
 		{
-			int count = 0;
+			count = 0;
 			while (pswap->a->content < v + 1)
 			{
 				count++;
 				apply(pswap, RA);
 			}
 			apply(pswap, PA);
-			while (count-- > 0)
-				apply(pswap, RRA);
+			while (count > 0)
+				apply(pswap, RRA - (count-- * 0));
 		}
 	}
+
+	// while (count < 0)
+	// 	apply(pswap, RA + count++ * 0);
+	// while (count > 0)
+	// 	apply(pswap, RRA - count-- * 0);
 }
 
 void	sort(t_dstack *pswap)
