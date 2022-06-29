b15d74c1350c66c91f1a30645d5b7bb02097798a
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-29 11:15:35 +0200

    just-rotate added: Fails: 0  KOs: 17  OKs: 53  Outstanding: 49

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -12,10 +12,114 @@
 
 #include "sort.h"
 
+/* just rotate */
+static int	ready_just_rotate(t_dstack *pswap)
+{
+	t_list	*tmp;
+	int		i;
+
+	if (pswap->b)
+		return (0);
+	if (is_sorted(pswap))
+		return (1);
+	tmp = pswap->a;
+	while (tmp && tmp->content != 0)
+		tmp = tmp->next;
+	i = 0;
+	while (tmp)
+	{
+		if (tmp->content != i++)
+			return (0);
+		tmp = tmp->next;
+	}
+	tmp = pswap->a;
+	while (i < pswap->size)
+	{
+		if (tmp->content != i++)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+static int	dist_to_sandwich(int v, t_list *lst)
+{
+	int 	dist;
+	t_list	*tmp;
+	t_list	*prev;
+
+	if (lst->content > v && ft_lstlast(lst)->content < v)
+		return (0);
+	dist = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		prev = tmp;
+		tmp = tmp->next;
+		dist++;
+		if (tmp && tmp->content >= v && prev->content < v)
+			return (dist);
+	}
+	printf("Error: dist_to_sandwich\n");
+	return (0);
+}
+
+static int	index_lst(int v, t_list *lst)
+{
+	int		i;
+	t_list	*tmp;
+
+	i = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		if (tmp->content == v)
+			return (i);
+		tmp = tmp->next;
+		i++;
+	}
+	return (0);
+}
+
+static int	dist_to(int v, t_list *lst)
+{
+	if (v <= get_from_lst(ft_min, lst))
+		return (index_lst(get_from_lst(ft_min, lst), lst));
+	if (v > get_from_lst(ft_max, lst))
+		return (ft_lstsize(lst) + 1);
+	return (dist_to_sandwich(v, lst));
+}
+
+static void	just_rotate(t_dstack *pswap, int l)
+{
+	t_list	*lst;
+	int		i;
+
+	lst = pswap->a;
+	if (l == 1)
+		lst = pswap->b;
+	i = dist_to(0, lst);
+	if (i <= ft_lstsize(lst) / 2)
+		while (i-- > 0)
+			apply(pswap, RA + l);
+	else
+	{
+		i = ft_lstsize(lst) - i;
+		while (i-- > 0)
+			apply(pswap, RRA + l);
+	}
+}
+
+/* just rotate */
+
+
 void	sort_5(t_dstack *pswap)
 {
 	int	s;
 	int	count;
+
+	if (ready_just_rotate(pswap))
+		return just_rotate(pswap, 0);
 
 	s = pswap->size;
 	while (s-- > 3)
