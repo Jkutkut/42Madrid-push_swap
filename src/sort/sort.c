aee692828ace32569c7fa252ad23847b9dbecb80
jkutkut <jre-gonz@student.42madrid.com>
2022-11-29 19:00:15 +0100

    bug where stack is sorted and input is large fixed

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 10:16:20 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/11/18 10:13:41 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/11/29 18:51:32 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
  */
 void	ft_sort(t_pswap *pswap)
 {
+	if (ft_ready_just_rotate(pswap))
+		return (ft_just_rotate(pswap, STACK_A));
 	if (pswap->size <= 5)
 		ft_sort_5(pswap);
 	else if (pswap->size <= 500)
